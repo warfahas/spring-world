1a19998c7651c5ba3b2f841656df7ee1919247c6
Mohamed Abdi <mkabdi27@gmail.com>
2018-10-31 20:48:16 -0500

    initial commit

---
+++ src/main/java/org/launchcode/springworld/SpringWorldApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.springworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWorldApplication.class, args);
+	}
+}
